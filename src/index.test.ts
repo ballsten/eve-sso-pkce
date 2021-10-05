7a922ca4c980887a982a2953682a2c1bb3498f5c
Andrew Theaker <andrew@theaker.name>
2021-10-05 20:43:49 +1100

    add linting

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,9 @@
 import { test } from '.'
 import { expect } from 'chai'
 
-describe("test", () => {
-  it("should pass", () => {
-    expect(window.crypto).to.exist
+describe('test', () => {
+  it('should pass', () => {
+    expect(window.crypto).to.be.an('object')
     expect(test()).to.equal('this is a test')
   })
 })
