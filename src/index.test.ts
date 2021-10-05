be3598b334e919009c012cf9d8ac6b1bc84f34a3
Andrew Theaker <andrew@theaker.name>
2021-10-05 21:17:36 +1100

    first test is working

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,30 @@
-import { test } from '.'
+import { createSSO } from '.'
 import { expect } from 'chai'
 
-describe('test', () => {
-  it('should pass', () => {
-    expect(window.crypto).to.be.an('object')
-    expect(test()).to.equal('this is a test')
+const CLIENT_ID = 'abcdef1234567890abcdef1234567890'
+const REDIRECT_URI = 'http://localhost/callback'
+
+describe('getUri', () => {
+  it('it will generate correct Uri', async () => {
+    const auth = createSSO({
+      method: 'pkce',
+      clientId: CLIENT_ID,
+      redirectUri: REDIRECT_URI
+    })
+
+    const scopes = [
+      'scope-1',
+      'scope-2'
+    ]
+
+    const uri = await auth.getUri(scopes)
+    const url = new URL(uri)
+
+    expect(url.protocol).to.equal('https:')
+    expect(url.host).to.equal('login.eveonline.com')
+    expect(url.pathname).to.equal('/v2/oauth/authorize')
   })
+
+  it('it sets the right paramaters')
+  it('it creates the right code_challenge')
 })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,67 @@
-export function test (): string {
-  return 'this is a test'
+import { isNode } from 'browser-or-node'
+
+interface EveSSOPCKEAuthConfig {
+  method: 'pkce'
+  clientId: string
+  redirectUri: string
+}
+
+export function createSSO (config: EveSSOPCKEAuthConfig): EveSSOAuth {
+  return new EveSSOAuth(config)
+}
+
+const BASE_URI = 'https://login.eveonline.com/'
+const AUTHORIZE_PATH = '/v2/oauth/authorize'
+
+async function getRandomString (length: number): Promise<string> {
+  const numBytes = Math.floor(length / 2)
+  if (isNode === true) {
+    const { randomBytes } = await import('crypto')
+    return randomBytes(numBytes).toString('hex')
+  } else {
+    const array = new Uint8Array(numBytes)
+    window.crypto.getRandomValues(array)
+    return ([...array].map(x => x.toString(16).padStart(2, '0')).join(''))
+  }
+}
+
+class EveSSOAuth {
+  protected config: EveSSOPCKEAuthConfig
+  protected states = new Map<string, string>()
+
+  constructor (config: EveSSOPCKEAuthConfig) {
+    this.config = config
+  }
+
+  async generateState (): Promise<string> {
+    return await getRandomString(8)
+  }
+
+  async generateCodeVerifier (): Promise<string> {
+    return await getRandomString(32)
+  }
+
+  async generateCodeChallenge (codeVerifier: string): Promise<string> {
+    return `${codeVerifier}-YOLO`
+  }
+
+  async getUri (scope: string[] = []): Promise<string> {
+    const state = await this.generateState()
+    const codeVerifier = await this.generateCodeVerifier()
+    const codeChallenge = await this.generateCodeChallenge(codeVerifier)
+
+    this.states.set(state, codeVerifier)
+
+    const url = new URL(AUTHORIZE_PATH, BASE_URI)
+
+    url.searchParams.append('response_type', 'code')
+    url.searchParams.append('redirect_uri', this.config.redirectUri)
+    url.searchParams.append('client_id', this.config.clientId)
+    url.searchParams.append('code_challenge', codeChallenge)
+    url.searchParams.append('code_challenge_method', 'S256')
+    url.searchParams.append('scope', scope.join(' '))
+    url.searchParams.append('state', state)
+
+    return url.toString()
+  }
 }
