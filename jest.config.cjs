80a1d20ddbefaff79f019b76906641ef8dc59e29
Andrew Theaker <andrew@theaker.name>
2022-06-21 20:19:22 +1000

    make changes to tests due to change to module

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: './test-environment.js'
+  testEnvironment: './test-environment.cjs'
 }
